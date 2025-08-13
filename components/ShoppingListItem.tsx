8c8c94b4259911d2aca8bf110f591afb1335cf19
ltn <ltn.sunrise@gmail.com>
2025-08-13 15:40:38 +0700

    Use icons for buttons

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -1,5 +1,7 @@
 import { TouchableOpacity, View, Alert, StyleSheet, Text } from 'react-native';
 import { theme } from '../theme';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 
 type Props = {
   name: string;
@@ -29,23 +31,27 @@ export function ShoppingListItem({ name, isCompleted }: Props) {
         isCompleted ? styles.completedContainer : undefined,
       ]}
     >
-      <Text
-        style={[
-          styles.itemText,
-          isCompleted ? styles.completedText : undefined,
-        ]}
-      >
-        {name}
-      </Text>
-      <TouchableOpacity
-        onPress={handleDelete}
-        style={[
-          styles.button,
-          isCompleted ? styles.completedButton : undefined,
-        ]}
-        activeOpacity={0.8}
-      >
-        <Text style={styles.buttonText}>Delete</Text>
+      <View style={styles.row}>
+        <Entypo
+          name={isCompleted ? 'check' : 'circle'}
+          size={24}
+          color={isCompleted ? theme.colorGrey : theme.colorCerulean}
+        />
+        <Text
+          style={[
+            styles.itemText,
+            isCompleted ? styles.completedText : undefined,
+          ]}
+        >
+          {name}
+        </Text>
+      </View>
+      <TouchableOpacity hitSlop={20} onPress={handleDelete}>
+        <AntDesign
+          name="closecircle"
+          size={24}
+          color={isCompleted ? theme.colorGrey : theme.colorRed}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -64,17 +70,13 @@ const styles = StyleSheet.create({
   itemText: {
     fontSize: 18,
     fontWeight: '200',
+    marginLeft: 8,
+    flex: 1,
   },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    letterSpacing: 1,
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
   },
 
   completedContainer: {
@@ -85,8 +87,5 @@ const styles = StyleSheet.create({
     color: theme.colorGrey,
     textDecorationLine: 'line-through',
     textDecorationColor: theme.colorGrey,
-  },
-  completedButton: {
-    backgroundColor: theme.colorGrey,
   },
 });
